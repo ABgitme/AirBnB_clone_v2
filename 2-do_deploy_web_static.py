9d22d7acd539f72a8d6de334b417cf74b11b643d
ABgitme <abebekezhabesha@gmail.com>
2024-04-07 06:00:01 +0300

    2-do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
 
+env.hosts = ["54.175.120.195", "54.226.17.144"]
+env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
@@ -17,12 +16,12 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, "/tmp/").failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
@@ -47,4 +46,3 @@ def do_deploy(archive_path):
            format(name)).failed is True:
         return False
     return True
-
