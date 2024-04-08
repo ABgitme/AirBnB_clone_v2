cfabafb96e45d4f4c3568ff9404d716a9a443b23
ABgitme <abebekezhabesha@gmail.com>
2024-04-08 05:16:49 +0300

    2-do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
-import os.path
+from os.path import exists
 from fabric.api import env, put, run
 
 env.hosts = ["54.175.120.195", "54.226.17.144"]
@@ -15,23 +15,22 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
     try:
         put(archive_path, "/tmp/")
-        run("rm -rf /data/web_static/releases/{}/".format(name))
         run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file, name))
         run("rm /tmp/{}".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(name, name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
         run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(name))
         return True
     except Exception as e:
-        print(e)
         return False
