8938cd9e805bf4953dc4d06d85166b2b3d5fed7a
ABgitme <abebekezhabesha@gmail.com>
2024-04-20 18:25:34 +0200

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# starts a Flask web application
+"""
+    starts a Flask web application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -17,6 +20,8 @@ def hello():
 
 
 if __name__ == '__main__':
-    # starts flack server
-    # listen on network 0.0.0.0:5000
+    """
+    starts flack server
+    listen on network 0.0.0.0:5000
+    """
     app.run(host='0.0.0.0', port=5000)
